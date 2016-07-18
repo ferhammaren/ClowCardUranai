c94df4a6a189a0933a3d8c8466659279ddab0dd5
Fer Meza <fernanda.meza.lopez@gmail.com>
2016-07-18 14:42:40 -0700

    Genesis of Aquarion

---
+++ ClowCardUranai.BLL/Models/ClowCard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClowCardUranai.BLL.Models
+{
+    class ClowCard
+    {
+        string cardName { get; set; }
+        string meaning1 { get; set; }
+        string meaning2 { get; set; }
+        string picture { get; set; }
+
+        public ClowCard(string cardName,string meaning1, string meaning2, string picture)
+        {
+            this.cardName = cardName;
+            this.meaning1 = meaning1;
+            this.meaning2 = meaning2;
+            this.picture = picture;
+        }
+        public ClowCard()
+        {
+            
+        }
+
+        public string drawCardBack()
+        {
+            //draw the card's back pattern
+            return "";
+        }
+    }
+}
---
+++ ClowCardUranai.DAL/DataAccess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClowCardUranai.DAL
+{
+    class DataAccess
+    {
+        public static void getCardData(int cardNumber)
+        {
+
+        }
+        public static void getSavedSpread(int spread) //can save in 5 spread slots
+        {
+
+        }
+        public static void saveSpread(int spread) //can read 5 spread slots
+        {
+
+        }
+
+    }
+}
